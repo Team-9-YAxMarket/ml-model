2733712b394e7019e71ef909f96a1c1efddef6b3
Olesia Zorich <evasvarienko@gmail.com>
2023-06-09 09:14:53 +0700

    Add files via upload

--- src/ml/model.py
+++ src/ml/model.py
@@ -1,56 +1,18 @@
-from typing import Any
+import pickle
+from catboost import CatBoostClassifier
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
+def predict(x):
+    # Загрузка модели из файла
+    with open("file.pcl", "rb") as fid:
+        loaded_model = pickle.load(fid)
 
-def predict(request: dict[Any]) -> dict[Any]:
-    """Return ML prediction.
+    # Загрузка обученного LabelEncoder из модели
+    label_encoder = loaded_model._le
 
-    request = {
-        "order": {
-            "order_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-            "items": [
-                {
-                    "sku": "unique_sku_1",
-                    "size_1": 0.1, "size_2": 0.2, "size_3": 0.3,
-                    "weight": 0.4,
-                    "type": [0, 1, 2],
-                },
-                {
-                    "sku": "unique_sku_2",
-                    "size_1": 1.1, "size_2": 1.2, "size_3": 1.3,
-                    "weight": 1.4,
-                    "type": [4, 5],
-                },
-            ],
-        },
-        "carton_leftovers": [
-            {"carton_type": "YMA", "amount": 34},
-            {"carton_type": "YMC", "amount": 20},
-            {"carton_type": "YMF", "amount": 30},
-            {"carton_type": "KSD", "amount": 0},
-            ...
-        ],
-    }
-    """
+    # Раскодирование предсказанных вероятностей
+    y_pred_proba = loaded_model.predict_proba(x)
+    y_pred = label_encoder.inverse_transform(np.argmax(y_pred_proba, axis=1))
 
-    ...
-
-    return {
-        "order_id": request["order"]["order_id"],
-        "packs": [
-            {
-                "packages": ["ABC", "DEF"],
-                "items": [
-                    {"sku": "unique_sku_1", "amount": 1},
-                    {"sku": "unique_sku_2", "amount": 1},
-                    {"sku": "unique_sku_3", "amount": 1},
-                ],
-            },
-            {
-                "packages": ["GHI", "JKL"],
-                "items": [
-                    {"sku": "unique_sku_2", "amount": 2},
-                    {"sku": "unique_sku_3", "amount": 1},
-                ],
-            },
-        ],
-    }
+    return y_pred
