07809f01174bd7f102b8a299a24c2270ca1afad5
profprogru <profprogru@gmail.com>
2023-06-14 14:45:39 +0300

    20230614-1

--- src/ml/model.py
+++ src/ml/model.py
@@ -2,18 +2,37 @@ import os
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OrdinalEncoder
+from catboost import CatBoostClassifier
 import pickle
+from pickle import load
 import joblib
+from skmultilearn.model_selection import iterative_train_test_split 
+
+
+MODEL_PATH = "C:\\pythons\\hack\\ml-model\\src\\ml\\"
 
 
 def predict(data):
-
+    
     # Загрузка модели из файла - это понятно, внутри катбустер
-    with open("./src/ml/model.pcl", "rb") as fid:
-        base_model = pickle.load(fid)
+    with open(MODEL_PATH+"model.pcl", "rb") as fid:
+        tun_model = pickle.load(fid)
 
     # Преобразование словарей в датафреймы
     items = data["items"]
+    
+    
+    manual = []
+    items_ = items.copy()
+    items = []
+    if len(items)>3:
+        for idx, x in enumerate(items_):
+            if idx<3:
+                items.append(x)
+            else:
+                manual.append(x)
+    
     df = pd.DataFrame(items)
     df["orderId"] = data["orderId"]
     df = df[["orderId", "sku", "count", "size1", "size2", "size3", "weight", "type"]]
@@ -22,7 +41,7 @@ def predict(data):
 
     # удалим часть информации о карготипах:
     df.drop(['type'], axis = 1, inplace = True)
-
+    
     # Создаем новый признак:
     df['pack_volume'] = df['size1'] * df['size2'] * df['size3']
 
@@ -31,26 +50,48 @@ def predict(data):
     'pack_volume': 'mean',
     'weight': 'mean',
     'sku': 'count'
-    }).reset_index()
-
+    }).reset_index() 
+    
     df_agg.rename(columns={'sku': 'item_count'}, inplace=True)
-
+    
+    # Проверяем значение item_count и возвращаем соответствующий результат
+    if df_agg['item_count'].max() > 3:
+        return 'рекомендована ручная сборка'
+    
     # сведения о товарах в заказе:
     order_agg = df.pivot_table(index='orderId', columns=df.groupby('orderId').cumcount()+1, aggfunc='first')
     order_agg.columns = [f'{col[0]}_{col[1]}' for col in order_agg.columns]
     order_agg = order_agg.reset_index()
 
-    # Объеденяем информацию о заказе
-    df_new = df_agg.merge(order_agg, how='inner', on = 'orderId')
+    # Объединяем информацию о заказе
+    df_new = df_agg.merge(order_agg, how='inner', on='orderId')
+
+    # Создаем функцию, которая добавит все недостающие столбцы для обучения модели:
+    def add_missing_columns(df_new):
+        all_columns = ['orderId', 'pack_volume', 'weight', 'item_count', 'count_1', 'count_2', 'count_3',
+                       'pack_volume_1', 'pack_volume_2', 'pack_volume_3', 'size1_1', 'size1_2', 'size1_3',
+                       'size2_1', 'size2_2', 'size2_3', 'size3_1', 'size3_2', 'size3_3', 'sku_1', 'sku_2', 'sku_3',
+                       'weight_1', 'weight_2', 'weight_3']
+        existing_columns = df_new.columns.tolist()
+        missing_columns = list(set(all_columns) - set(existing_columns))
+        for col in missing_columns:
+            df_new[col] = 0
+        df_new = df_new.reindex(columns=all_columns)
+        return df_new 
+
+    df_new = add_missing_columns(df_new)
+
+    # Удаляем лишние столбцы
     df_new.drop(['orderId', 'sku_1', 'sku_2', 'sku_3'], axis=1, inplace=True)
 
     # ПРЕДСКАЗАНИЕ МОДЕЛИ
-    y_pred = base_model.predict_proba(df_new)
+    y_pred = tun_model.predict_proba(df_new)
 
-    # заскодировка предсказания:
-    encoder = joblib.load('model_encoder.pkl')
+    # pаскодировка предсказания:
+    encoder = joblib.load(MODEL_PATH+'model_encoder.pkl')
     predicted_labels = np.argmax(y_pred, axis=1)
     y_pred_norm = encoder.inverse_transform(predicted_labels.reshape(-1, 1))
+ 
 
     ###############
     response = {
@@ -62,7 +103,7 @@ def predict(data):
         {"sku": "unique_sku_3", "add_packs": []},
       ],
       "no_room_for": [
-        {"sku": "unique_sku_4", "add_packs": ['пузырьки']},
+        manual
       ],
       "status": "ok"
     }
--- src/ml/test.py
+++ src/ml/test.py
@@ -1,26 +1,52 @@
 import requests
 
-# data = {
-#     "orderId": "unique_order_id",
-#     "items": [
-#     {"sku": "unique_sku_1", "count": 1, "size1": "5.1", "size2": "2.2", "size3": "5.3",
-#      "weight": "7.34", "type": ["2"]},
-#     {"sku": "unique_sku_2", "count": 3, "size1": "4", "size2": "5.23", "size3": "6.2",
-#      "weight": "7.45", "type": ["8", "9", "10"]},
-#     {"sku": "unique_sku_3", "count": 2, "size1": "11", "size2": "12.5", "size3": "13.3",
-#      "weight": "14.2", "type": ["15", "16"]}
-#     ]
-# }
 data = {
-    "orderid'": "unique_order_id",
-    "package": "saddad",
+    "orderId": "unique_order_id",
     "items": [
-        {"sku": "unique_sku_1", "add_packs": ['пузырьки']},
-        {"sku": "unique_sku_2", "add_packs": ['стекло','отдельная упаковка']},
-        {"sku": "unique_sku_3", "add_packs": []},
-    ],
-    "no_room_for": []
+    {"sku": "unique_sku_1", "count": 1, "size1": "5.1", "size2": "2.2", "size3": "5.3",
+     "weight": "7.34", "type": ["2"]},
+    {"sku": "unique_sku_2", "count": 3, "size1": "4", "size2": "5.23", "size3": "6.2",
+     "weight": "7.45", "type": ["8", "9", "10"]},
+    {"sku": "unique_sku_3", "count": 2, "size1": "11", "size2": "12.5", "size3": "13.3",
+     "weight": "14.2", "type": ["15", "16"]}
+    ]
 }
+
+r = requests.post("http://localhost:8080/pack", json=data)
+
+print(r.json())
+print()
+print('*'*50)
+
+data = {
+    "orderId": "unique_order_id",
+    "items": [
+    {"sku": "unique_sku_1", "count": 1, "size1": "5.1", "size2": "2.2", "size3": "5.3",
+     "weight": "7.34", "type": ["2"]}
+    ]
+}
+
+r = requests.post("http://localhost:8080/pack", json=data)
+
+print(r.json())
+print()
+print('*'*50)
+      
+
+data = {
+    "orderId": "unique_order_id",
+    "items": [
+    {"sku": "unique_sku_1", "count": 1, "size1": "5.1", "size2": "2.2", "size3": "5.3",
+     "weight": "7.34", "type": ["2"]},
+    {"sku": "unique_sku_2", "count": 3, "size1": "4", "size2": "5.23", "size3": "6.2",
+     "weight": "7.45", "type": ["8", "9", "10"]},
+    {"sku": "unique_sku_3", "count": 2, "size1": "11", "size2": "12.5", "size3": "13.3",
+     "weight": "14.2", "type": ["15", "16"]},
+    {"sku": "unique_sku_4", "count": 2, "size1": "11", "size2": "12.5", "size3": "13.3",
+     "weight": "14.2", "type": ["15", "16"]}
+    ]
+}
+
 r = requests.post("http://localhost:8080/pack", json=data)
 
 print(r.json())
